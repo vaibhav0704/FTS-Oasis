53030803a3ea9536fbb6be8e53884a146807efd3
vaibhav0704 <vaibhavsaraf.08@gmail.com>
2021-08-16 22:52:13 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const cookieParser = require("cookie-parser");
-// const csrf = require("csurf");
 const bodyParser = require("body-parser");
 const express = require("express");
 const admin = require("firebase-admin");
@@ -8,78 +6,81 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://oasis-81f3e-default-rtdb.asia-southeast1.firebasedatabase.app",
 });
 
-// const csrfMiddleware = csrf({ cookie: true });
+const db = admin.firestore();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.engine("html", require("ejs").renderFile);
+app.set('view engine', 'ejs');
 app.use(express.static("static"));
+app.use(express.urlencoded({
+  extended: true
+}))
 
 app.use(bodyParser.json());
-app.use(cookieParser());
-// app.use(csrfMiddleware);
-app.use(express.static(__dirname + '/public'));
-
-// app.all("*", (req, res, next) => {
-//   res.cookie("XSRF-TOKEN", req.csrfToken());
-//   next();
-// });
 
 app.get("/login", function (req, res) {
   res.render("login.html");
 });
 
 app.get("/signup", function (req, res) {
-  res.render("signup.html");
+  res.render("signup");
 });
 
 app.get("/profile", function (req, res) {
   res.render("profile.html");
-  // const sessionCookie = req.cookies.session || "";
-
-  // admin
-  //   .auth()
-  //   .verifySessionCookie(sessionCookie, true /** checkRevoked */)
-  //   .then(() => {
-  //     res.render("profile.html");
-  //   })
-  //   .catch((error) => {
-  //     res.redirect("/login");
-  //   });
 });
 
 app.get("/", function (req, res) {
   res.render("login.html");
 });
 
-// app.post("/sessionLogin", (req, res) => {
-//   const idToken = req.body.idToken.toString();
+app.post('/createUser', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.pass;
+  const name = req.body.name;
+  const fatherName = req.body.fatherName;
+  const regNo = req.body.regNo;
+  const startReg = regNo.substring(0, 3)
+  const phone = req.body.phone;
+  const age = req.body.age;
+  const repass = req.body.re_pass;
 
-//   const expiresIn = 60 * 60 * 24 * 5 * 1000;
+  if(password === repass){
+    if(password.length >= 8){
+      if(phone.length === 10){
+        if(regNo.length === 8 && startReg === '121'){
+          admin
+          .auth()
+          .createUser({
+            email,    
+            password: password,
+          })
+          .then((userRecord) => {
+            
+          })
+          .catch((error) => {
+            console.log('Error creating new user:', error);
+          });
+        }else{
+          res.status(401).send("Registration number provided does not belong to freshmen student")
+        }        
+      }else{
+        res.status(401).send("incorrect phone number provided")
+      }
+    }else{
+      res.status(401).send("Password length should be equal to or greater than 8 characters")
+    }
+  }else{
+    res.status(401).send("password does not match the confirmed password")
+  }
+  
+ 
+})
 
-//   admin
-//     .auth()
-//     .createSessionCookie(idToken, { expiresIn })
-//     .then(
-//       (sessionCookie) => {
-//         const options = { maxAge: expiresIn, httpOnly: true };
-//         res.cookie("session", sessionCookie, options);
-//         res.end(JSON.stringify({ status: "success" }));
-//       },
-//       (error) => {
-//         res.status(401).send("UNAUTHORIZED REQUEST!");
-//       }
-//     );
-// });
-
-// app.get("/sessionLogout", (req, res) => {
-//   res.clearCookie("session");
-//   res.redirect("/login");
-// });
+app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
